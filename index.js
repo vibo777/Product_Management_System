83ea86b8de074eb4796b8a7945b5bceacf1da24d
vibo777 <54715728+vibo777@users.noreply.github.com>
2021-10-04 16:16:33 +0530

    Project Finished
    Completed with the update & delete functionality of Product management system. i.e CRUD APP

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let products=[
     {name: "JBL Sound Bar",price:4999,quantity:1},
 ];   // blank array for storing the products 
 
+
+// Add Functionality to Product product  
 function addProduct(){
 
     let product={};  // 1. first create one object  
@@ -15,18 +17,25 @@ function addProduct(){
  
     // 3. Push object into an array 
     products.push(product);
- //   console.log(products) // simply printing array to check array
+    // console.log(products) // simply printing array to check array
     display();
 
     // to clear the input fields after inserting record (Setters:Setting the value)
     document.getElementById('name').value = "";
-    product.price = document.getElementById('price').value = "";
+    document.getElementById('price').value = "";
     document.getElementById('quantity').value = "";
 
 
 
 }
 
+// Delete Functionality to remove products  
+deleteProduct=(index)=>{
+    products.splice(index,1);
+    display();
+}
+
+// Functionality to Display the list of products
 function display(){
     // important logic 
 
@@ -40,18 +49,52 @@ function display(){
             <td>${products[i].price}</td>
             <td>${products[i].quantity}</td>  
             <td>
-                <button class="btn">Update</button>
-                <button class="btn">Delete</button>
+                <button class="btn" onclick='setUpData(${i})'>Update</button>
+                <button class="btn" onclick='deleteProduct(${i})'>Delete</button>
             </td>
         </tr>   
         `; 
     }
     document.getElementById('table_data').innerHTML = productString;
-
 }    
 display();
+ 
+let updateIndex = null;
 
+setUpData=(index)=>{
 
+    document.getElementById('update_form').style.display="block";
+    updateIndex = index;
+
+    let product = products[index];    
+     console.log(product);
+  // dom manipulation 
+  // 1. to fetch the element  
+
+        document.getElementById('up_name').value = product.name;
+        document.getElementById('up_price').value = product.price;
+        document.getElementById('up_quantity').value = product.quantity;
+
+}
+
+updateProduct=()=>{
+     
+    let product={};    
+     
+    product.name = document.getElementById('up_name').value;
+    product.price = document.getElementById('up_price').value;
+    product.quantity = document.getElementById('up_quantity').value;
+
+    products[updateIndex] = product;
+    display();
+
+    document.getElementById('up_name').value = "";
+    document.getElementById('up_price').value = "";
+    document.getElementById('up_quantity').value = "";
+
+    document.getElementById('update_form').style.display="none";
+    updateIndex = null; 
+}
 
 
 
